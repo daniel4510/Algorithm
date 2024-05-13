d3140a6386cd66f6bfa75a20154c698f02b1e9b6
daniel4510 <59697638+daniel4510@users.noreply.github.com>
2024-05-13 20:03:04 +0900

    [level 1] Title: 옹알이 (2), Time: 2.12 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -0,0 +1,22 @@
+class Solution {
+    public static int solution(String[] babbling) {
+        int answer = 0;
+
+        for(String word : babbling){
+            if(word.contains("ayaaya") || word.contains("yeye") || word.contains("woowoo") || word.contains("mama"))
+                continue;
+
+            word = word.replace("aya"," ");
+            word = word.replace("ye"," ");
+            word = word.replace("woo"," ");
+            word = word.replace("ma"," ");
+            word = word.replace(" ","");
+
+            System.out.println(word);
+
+            if(word.length() == 0)
+                answer++;
+        }
+        return answer;
+    }
+}
