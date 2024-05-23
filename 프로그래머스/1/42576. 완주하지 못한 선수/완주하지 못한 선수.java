f68b33efb4cfca60a2e55289b357a7ce529432c8
daniel4510 <59697638+daniel4510@users.noreply.github.com>
2024-05-23 22:39:10 +0900

    Update 완주하지 못한 선수.java

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,21 +2,15 @@ import java.util.Arrays;
 
 class Solution {
     public static String solution(String[] participant, String[] completion) {
-        String answer = "";
         Arrays.sort(participant);
         Arrays.sort(completion);
 
-        for (int i = 0; i < participant.length; i++) {
-            if(i == completion.length){
-                answer = participant[i];
-                break;
-            }
+        for (int i = 0; i < completion.length; i++) {
             if (!(participant[i].equals(completion[i]))){
-                answer = participant[i];
-                break;
+                return participant[i];
             }
         }
         
-        return answer;
+        return participant[participant.length - 1];
     }
 }
